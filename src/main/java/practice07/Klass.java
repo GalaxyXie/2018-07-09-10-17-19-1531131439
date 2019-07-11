904830b6b2ff4e98b38a4daa19cd6212fba42122
GalaxyXie <771472141@qq.com>
2019-07-11 21:28:17 +0800

    feat:implement practice07

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,22 @@
 package practice07;
 
 public class Klass {
+    private int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+    public String getDisplayName(){
+        if(this.getNumber()==0)
+            return "No Class";
+        else
+            return "Class "+this.getNumber();
+    }
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,49 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+    private Student student;
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Teacher(String name, int age, Klass klass, Student student) {
+        super(name, age);
+        this.klass = klass;
+        this.student = student;
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+       if(this.getKlass()==null) {
+           return super.introduce() + " I am a Teacher. I teach No Class.";
+       }else{
+           return super.introduce()+" I am a Teacher. I teach "+this.getKlass().getDisplayName()+".";
+       }
+
+    }
+    public String introduceWith(Student student){
+        if(student.getKlass().getNumber()==this.getKlass().getNumber()){
+        return  super.introduce()+" I am a Teacher. I teach "+student.getName()+".";
+        }else {
+            return super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
 }
