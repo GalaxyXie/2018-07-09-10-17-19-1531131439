c18169487a4fb7c4c770e38b298722e375bd1b20
GalaxyXie <771472141@qq.com>
2019-07-11 20:42:53 +0800

    feat:implement practice02

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,22 @@
 package practice02;
 
 public class Person {
+    private String name;
+    private int age;
+    public String introduce(){
+
+        return "My name is "+this.getName()+ ". I am "+this.getAge()+" years old.";
+    }
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
